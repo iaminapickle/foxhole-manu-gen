3401516fc002dd318a267b70c808db57fa04610c
samuele-yang <samuele2001@hotmail.com>
2025-09-28 02:07:43 +1000

    Basic n-batch search implemented

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod cost_metric;
 use std::{collections::HashMap, fs::File, time::Instant, fmt::Write as fmtWrite, io::Write as ioWrite};
 
 use crate::{category::{material_grouped_warden_category::MaterialGroupedWardenCategory, warden_category::WardenCategory, Category}, cost_metric::CostMetric};
+use bit_vec::BitVec;
 use nalgebra::{ArrayStorage, Dyn, Matrix, VecStorage, U1, U4, U7};
 use strum::IntoEnumIterator;
 
 type CostVec = Matrix<u16, U1, NoMaterials, ArrayStorage<u16, 1,  NO_MATERIALS>>;
 type QueueVec = Matrix<u16, U1, Dyn, VecStorage<u16, U1, Dyn>>;
+type QueueGroup = (Vec<(QueueVec)>, BitVec);
 type Batch = Vec<QueueVec>;
 
 type NoCategories = U7;
@@ -35,7 +37,7 @@ pub fn format_batch(batch: Batch) -> String {
     let mut res: String = String::new();
     let order: Vec<MaterialGroupedWardenCategory> = MaterialGroupedWardenCategory::iter().collect();
 
-    for c in 0..NO_CATEGORIES {
+    for c in 0..batch.len() {
         let category = &order[c];
         let names = category.item_order();
         
@@ -63,7 +65,7 @@ pub fn format_batch(batch: Batch) -> String {
     return res;
 }
 
-pub fn find_all_batches_with_metrics(metrics: Vec<CostMetric>) {
+pub fn find_n_batches_with_metrics(n: u8, metrics: Vec<CostMetric>) {
     let mut base_queues: HashMap<MaterialGroupedWardenCategory, Vec<(QueueVec, CostVec)>> = HashMap::new();
     for c in MaterialGroupedWardenCategory::iter() {
         base_queues.insert(c.clone(), c.generate_valid_queue_vecs());
@@ -76,11 +78,11 @@ pub fn find_all_batches_with_metrics(metrics: Vec<CostMetric>) {
         stack.push((vec![q], c));
     }
 
-    let path = format!("all_batches_with_{:?}.txt", metrics);
+    let path = format!("{n}_batches_with_{:?}.txt", metrics);
     let mut output = File::create(path).unwrap();
     while !stack.is_empty() {
         let cur: (Batch, CostVec) = stack.pop().unwrap();
-        if cur.0.len() == NO_CATEGORIES {
+        if cur.0.len() == n.into() {
             if metrics.iter().all(|m| m.check_metric(cur.1)) {
                 let _ = write!(output, "Batch: {}\nCost: {}\n", format_batch(cur.0), format_cost_vector(cur.1));
             }
@@ -98,12 +100,56 @@ pub fn find_all_batches_with_metrics(metrics: Vec<CostMetric>) {
     }
 }
 
+pub fn find_all_batches_with_metrics(metrics: Vec<CostMetric>) {
+    find_n_batches_with_metrics(NO_CATEGORIES.try_into().unwrap(), metrics);
+}
+
 fn main() {
     let now = Instant::now();
     let metrics: Vec<CostMetric> = Vec::from([
         CostMetric::PerfectlyCrateable(TRUCK_SIZE.try_into().unwrap()),
-        CostMetric::PerfectlyStackable(TRUCK_SIZE.try_into().unwrap())
+        // CostMetric::PerfectlyStackable(TRUCK_SIZE.try_into().unwrap())
     ]);
-    find_all_batches_with_metrics(metrics);
+    find_n_batches_with_metrics(2, metrics);
     println!("Elapsed: {:.2?}", now.elapsed());
 }
+
+// pub fn find_n_groups_with_metrics(n: u8, metrics: Vec<CostMetric>) {
+//     let mut base_queues: HashMap<MaterialGroupedWardenCategory, Vec<(QueueVec, CostVec)>> = HashMap::new();
+//     for c in MaterialGroupedWardenCategory::iter() {
+//         base_queues.insert(c.clone(), c.generate_valid_queue_vecs());
+//     }
+    
+//     let order: Vec<MaterialGroupedWardenCategory> = MaterialGroupedWardenCategory::iter().collect();
+    
+//     let mut stack: Vec<(QueueGroup, CostVec)> = Vec::new();
+//     for i in 0..NO_CATEGORIES {
+//         let queues = base_queues[&order[i]];
+//         let bv = BitVec::from_elem(NO_CATEGORIES, false);
+//         bv[i] = true;
+//         for (q, c) in queues {
+//             stack.push(((vec![q], bv), c))
+//         }
+//     }
+
+//     let path = format!("all_batches_with_{:?}.txt", metrics);
+//     let mut output = File::create(path).unwrap();
+//     while !stack.is_empty() {
+//         let cur: (Batch, CostVec) = stack.pop().unwrap();
+//         if cur.0.len() == NO_CATEGORIES {
+//             if metrics.iter().all(|m| m.check_metric(cur.1)) {
+//                 let _ = write!(output, "Batch: {}\nCost: {}\n", format_batch(cur.0), format_cost_vector(cur.1));
+//             }
+//             continue;
+//         }
+
+//         let next = order[cur.0.len()].clone();
+//         for (q,c) in base_queues[&next].clone() {
+//             if CostMetric::Affordable.check_metric(cur.1 + c) {
+//                 let mut tmp = cur.0.clone();
+//                 tmp.push(q);
+//                 stack.push((tmp, cur.1 + c));
+//             }
+//         }
+//     }
+// }
